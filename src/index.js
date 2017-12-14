d4c60dd6a51eb796737c46fcea78f7dc285a1164
Clement Hoang <clemmy@fb.com>
2017-12-14 15:17:26 -0800

    Add support for early breaks

--- src/index.js
+++ src/index.js
@@ -6,10 +6,19 @@ module.exports = function({types: t}) {
          * Wraps the body in a labelled block, wrapped by a do expression, which is returned in an immediately invoked generator function
          */
         exit(path) {
+          const labelIdentifier = path.scope.generateUidIdentifier("label");
           const gexpContents = path.get("body");
+
+          path.traverse(GeneratorExpressionVisitor, {labelIdentifier});
+
+          const labeledStatement = t.labeledStatement(labelIdentifier, gexpContents.node);
           const blockReturningDoExpr = t.blockStatement([
             t.returnStatement(
-              t.doExpression(gexpContents.node)
+              t.doExpression(
+                t.blockStatement([
+                  labeledStatement
+                ])
+              )
             )
           ]);
           const generatorFnc = t.functionExpression(null, [], blockReturningDoExpr, true, false);
@@ -20,3 +29,24 @@ module.exports = function({types: t}) {
     }
   };
 }
+
+const BREAKABLE_CONTAINER_NODE_TYPES = [
+      "GeneratorExpression",
+      "ForStatement",
+      "DoWhileStatement",
+      "WhileStatement",
+      "ForInStatement",
+      "ForOfStatement"
+    ];
+
+const GeneratorExpressionVisitor = {
+  BreakStatement: {
+    exit(path) {
+      const firstHit = path.find(p => BREAKABLE_CONTAINER_NODE_TYPES.includes(p.node.type));
+      if (!firstHit || !firstHit.isGeneratorExpression() || !this.labelIdentifier) {
+        return;
+      }
+      path.node.label = this.labelIdentifier;
+    }
+  }
+};
